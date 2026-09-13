59f4d31e082f22b202a08b48e86d57eff010ca16
ferreiracaique <ferreira.caique@escolar.ifrn.edu.br>
2026-09-13 18:02:20 -0300

    Alteração ProjEuler3

--- ProjetoEuler6.py
+++ ProjetoEuler6.py
@@ -1,5 +1,13 @@
 x = 0
-while x <= 10:
-    print (x)
+SomaQuad = 0
+Soma = 0
+QuadSoma = 0
+while x <= 100:
+    SomaQuad = SomaQuad + x * x
+    Soma = Soma + x
+    QuadSoma = Soma * Soma
     x = x + 1
+print (SomaQuad)
+print (QuadSoma)
+print (QuadSoma - SomaQuad)
     
