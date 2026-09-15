dafa19964678c383314e74321a4f325c76c0d5fa
ferreiracaique <ferreira.caique@escolar.ifrn.edu.br>
2026-09-15 17:30:30 -0300

    criei primeira versao do PrjetoEuler10

---
+++ ProjetoEuler10.py
@@ -0,0 +1,19 @@
+soma = 0
+numero = 2
+
+while numero < 100:
+    divisor = 2
+    quantidade_divisores = 0
+
+    while divisor < numero:
+        if numero % divisor == 0:
+            quantidade_divisores = quantidade_divisores + 1
+
+        divisor = divisor + 1
+
+    if quantidade_divisores == 0:
+        soma = soma + numero
+
+    numero = numero + 1
+
+print("A soma dos números primos abaixo de 100 é:", soma)
